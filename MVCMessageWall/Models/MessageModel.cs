1194f50ad4029cb7c493f8e1a5f82b8931bb1f11
David Chuvik <dchuvik@gmail.com>
2023-03-15 14:46:42 -0400

    loggin page

---
+++ MVCMessageWall/Models/MessageModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCMessageWall.Models
+{
+    public class MessageModel
+    {
+        [Microsoft.Build.Framework.Required]
+        [StringLength(10, MinimumLength = 5)]
+        [Display(Name = "Really cool Message")]
+        public string Message { get; set; }
+    }
+}
